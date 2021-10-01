691e18715130b3f4ef9b7b508eddb861fa554334
Wesley Ribeiro <wesleyaraujo@cc.ci.ufpb.br>
2021-10-01 18:45:40 -0300

    arquivos iniciais

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,9 @@
+export function Header() {
+  return (
+    <header>
+      <div>
+        <img src="/images/logo.svg" alt="ig.news" />
+      </div>
+    </header>
+  )
+}
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,7 +5,8 @@ export default class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <title>ig.news</title>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700;900&display=swap" rel="stylesheet" />
         </Head>
         <body>
           <Main />
